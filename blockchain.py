785e8c92991b671b8e537bca73d51ab3e3e7ca4e
Shashwat Nayak <shashwatnayak@outlook.com>
2018-01-13 16:45:15 +0530

    blueprint ready

---
+++ blockchain.py
@@ -0,0 +1,23 @@
+def blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        #new block and add it to chain
+        pass
+
+    def new_chain(self):
+        # Adds transaction to transactions
+        pass
+
+    @staticmethod
+    def hash(block):
+        # hashes the blocks
+        pass
+
+
+    @property
+    def last_block(self):
+        # returns last blocks
+        pass
